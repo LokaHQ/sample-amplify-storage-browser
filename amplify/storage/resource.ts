91ff28f7a019061a390e0d9310a0990624176eb4
dragansavevski <dragan@loka.com>
2025-04-24 15:34:06 +0200

    chore: update permissions

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -3,37 +3,30 @@ import { defineStorage } from '@aws-amplify/backend';
 export const storage = defineStorage({
   name: 'myStorageBucket',
   isDefault: true,
-   access: (allow) => ({
+  access: (allow) => ({
     'public/*': [
-        allow.guest.to(['read', 'write']),
-        allow.authenticated.to(['read', 'write', 'delete']),
+      allow.guest.to(['read', 'write']),
+      allow.authenticated.to(['read', 'write', 'delete']),
+      allow.groups(['admin']).to(['read', 'write', 'delete']),
     ],
     'admin/*': [
-        allow.groups(['admin']).to(['read', 'write', 'delete']),
-        allow.authenticated.to(['read'])
+      allow.authenticated.to(['read']),
+      allow.groups(['admin']).to(['read', 'write', 'delete']),
     ],
-    'private/{entity_id}/*': [
-        allow.entity('identity').to(['read', 'write', 'delete'])
-    ]
-   })
+  }),
 });
 
 export const secondaryStorage = defineStorage({
   name: 'mySecondaryStorageBucket',
-   access: (allow) => ({
+  access: (allow) => ({
     'backup_public/*': [
-        allow.guest.to(['read', 'write']),
-        allow.authenticated.to(['read', 'write', 'delete']),
+      allow.guest.to(['read', 'write']),
+      allow.authenticated.to(['read', 'write', 'delete']),
+      allow.groups(['admin']).to(['read', 'write', 'delete']),
     ],
     'backup_admin/*': [
-        allow.groups(['admin']).to(['read', 'write', 'delete']),
-        allow.authenticated.to(['read'])
+      allow.authenticated.to(['read']),
+      allow.groups(['admin']).to(['read', 'write', 'delete']),
     ],
-    'backup_private/{entity_id}/*': [
-        allow.entity('identity').to(['read', 'write', 'delete'])
-    ]
-   })
+  }),
 });
-
-
-
